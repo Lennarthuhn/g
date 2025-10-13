a62c3e08dbac6f1a672a7eeecf9bdd367e9d6c65
Lennarthuhn <122010877+Lennarthuhn@users.noreply.github.com>
2025-10-13 13:39:14 +0200

    .

---
+++ geyser/start.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar Geyser-Standalone.jar
+pause
---
+++ minecraft/autostart.bat
@@ -0,0 +1,27 @@
+@echo off
+echo ================================
+echo   Minecraft Server + Playit Autostart
+echo ================================
+
+:: Warte kurz, bis System bereit ist
+timeout /t 5 >nul
+
+:: Starte Minecraft-Server (im eigenen Fenster)
+echo [1/2] Starte Minecraft-Server...
+start "Minecraft Server" java -Xmx2G -Xms1G -jar "C:\Users\Huhn\odrive\OneDrive-365 for Business\Apps\minecraft\paper.jar" nogui
+
+:: Warte 10 Sekunden, damit der Server richtig läuft
+echo [Warte 10 Sekunden, bis Server läuft...]
+timeout /t 10 >nul
+
+start "Geyser" cmd /k java -jar "C:\Users\lstal\Downloads\geyser\Geyser-Standalone.jar"
+
+echo [Warte 10 Sekunden, bis geyser läuft...]
+timeout /t 10 >nul
+
+:: Starte Playit.gg
+echo [2/2] Starte Playit.gg Tunnel...
+start "" "C:\Program Files\playit_gg\bin\playit.exe"
+
+echo Fertig! Server laeuft.
+pause
