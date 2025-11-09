5a1dd6bd375eb775f63cd187ceeefab298591840
Lennarthuhn <122010877+Lennarthuhn@users.noreply.github.com>
2025-11-09 21:44:49 +0100

    .

--- minecraft/autostart.bat
+++ minecraft/autostart.bat
@@ -8,7 +8,7 @@ timeout /t 5 >nul
 
 :: Starte Minecraft-Server (im eigenen Fenster)
 echo [1/2] Starte Minecraft-Server...
-start "Minecraft Server" java -Xmx2G -Xms1G -jar paper.jar nogui
+start "Minecraft Server" java -Xmx3G -Xms1G -jar paper.jar nogui
 
 :: Warte 10 Sekunden, damit der Server richtig läuft
 echo [Warte 10 Sekunden, bis Server läuft...]
--- minecraft/start.bat
+++ minecraft/start.bat
@@ -1,3 +1,2 @@
 @echo off
-java -Xmx2G -Xms1G -jar paper-1.21.8-60.jar
-pause
+java -Xmx2G -Xms1G -jar paper.jar
